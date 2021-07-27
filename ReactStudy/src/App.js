37c5c7e3eceefa4aa09f99111442abd67afb56d7
kimud6003 <kimud6003@google.com>
2021-07-27 22:32:25 +0900

    주석 추가

--- ReactStudy/src/App.js
+++ ReactStudy/src/App.js
@@ -6,28 +6,27 @@ import ArrayRender from './component/arrayRender'
 import './App.css';
 
 function App(props) {
-  const nextId = useRef(4);
-  const [inputs, setInputs] = useState({
+  const nextId = useRef(4); //더미데이터의 길이가 3이기 때문에 4부터 시작
+  const [inputs, setInputs] = useState({ //Input State 설정
     car : "",
     number : ""
   })
-  const [Cars, setCars] = useState([...Dummy])
+  const [Cars, setCars] = useState([...Dummy]) // Dummy데이터를 state로 설정해 변화에 반응하게 세팅
   const onChange = (e) =>{
-    const {name, value}= e.target;
+    const {name, value}= e.target; // e.target에 있는 name과 value를 들고 온다 
     setInputs({
       ...inputs,
       [name] : value
     })
   }
   const {car,number} = inputs;
-  const onCreate = () => {
-    const Car = {
+  const onCreate = () => { //동적 생성 
+    const Car = { //현재 Ref의 값과, input값들을 넣어준다 
       id : nextId.current,
       car,
       number
     };
     setCars(Cars.concat(Car));
-    console.log(Cars)
     setInputs({
       car:'',
       number:''
@@ -43,7 +42,6 @@ function App(props) {
         car.id === id ? { ...car, active: !car.active } : car
       )
     );
-    console.log(Cars);
   };
   return (
     <div className="App">
